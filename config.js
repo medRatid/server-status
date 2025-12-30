6a844f347135a4ea6f356b62cb071a9553574afc
Mohammed RATID <mohammed.ratid@gmail.com>
2025-12-30 03:51:33 +0100

    Updates

--- config.js
+++ config.js
@@ -1,16 +1,1 @@
-const domain = "medratid.com";
-
-const sites = [
-    {
-        name: "CV Website",
-        url: "https://" + domain
-    },
-    {
-        name: "Splitter",
-        url: "https://splitter." + domain
-    },
-    {
-        name: "Overleaf",
-        url: "https://overleaf." + domain
-    }
-];
+const domain="medratid.com",sites=[{name:"CV Website",url:"https://"+domain},{name:"Splitter",url:"https://splitter."+domain},{name:"Overleaf",url:"https://overleaf."+domain}];
